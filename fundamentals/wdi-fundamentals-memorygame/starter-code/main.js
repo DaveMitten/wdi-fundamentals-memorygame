7d6ae4f230b097d5b91be5ab21b929dd277c1b2c
DaveMitten <davidmitten88@gmail.com>
2017-03-08 18:25:14 +0000

    Add files via upload

---
+++ fundamentals/wdi-fundamentals-memorygame/starter-code/main.js
@@ -0,0 +1,43 @@
+
+/*var CardOne = "Queen"
+var CardTwo = "Queen"
+var CardThree = "King"
+var CardFour = "King"
+
+if (CardOne === CardTwo){
+	alert = "You've found a match!";
+	}
+	else{
+		alert='Sorry try again!'
+	}
+if (CardThree === CardFour){
+	alert="You've found a match!";
+	}
+	else{
+		alert='Sorry try again!';
+	} */
+
+//creating an id with the 'game-board' and setting to a variable
+
+var game-board = document.getElementById('game-board')
+	//create a loop cycling through the cars ( not sure if it ineeds to be 3 or 4)
+	
+//this needs more explaining, no idea here! 
+	for (var i=0; i<3; i++) {
+
+		//here on completely confused, need to review how this section of the code works.
+		// create a div element which will be used as a card
+    var cardElement = document.createElement('div');
+
+    // add a class to the card element which will help link styling
+    cardElement.className = 'card';
+//I hope I am not actually changing achild into a card.
+    // append the card to the board
+    board.appendChild(cardElement);
+  }
+
+
+
+	}
+	
+
